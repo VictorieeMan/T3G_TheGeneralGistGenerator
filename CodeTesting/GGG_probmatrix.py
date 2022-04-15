cf7c1e9a051674129510971637d2301f624a76ee
Simon Jorstedt <jorstedtsimon@gmail.com>
2022-04-15 15:23:40 +0200

    Added Prob matrix testing code
    Wrote a program that does basically what we want to do. It calculates probabilities of words following other words.

---
+++ CodeTesting/GGG_probmatrix.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  3 17:26:18 2022
+
+@author: jorst
+"""
+
+import pandas
+
+
+def make_pmatrix(raw_text): #Ca 40 rows of code
+    "CREATE DATAFRAME TEMPLATE"
+    a_dict = {}
+    split_text = raw_text.split(" ")
+    unique_words = list(dict.fromkeys(split_text))
+    
+    for i in unique_words:
+        a_dict[i] = list(0 for i in range(len(unique_words)))
+    dframe = pandas.DataFrame(a_dict, index=unique_words)
+    
+    output_aid("Dataframe Template", dframe)
+    
+    
+    "COUNT OCCURENCES OF WORDS FOLLOWING OTHER WORDS"
+    last_word = split_text[0]
+    
+    split_text_wout_first = split_text
+    split_text_wout_first.pop(0) # Let's us start counting at second word.
+    
+    for new_word in split_text_wout_first:
+        # [column][row]
+        dframe[new_word][last_word] += 1
+        last_word = new_word
+    
+    output_aid("Dataframe of COUNTS", dframe)    
+    
+    
+    "TURN WORD COUNTS TO PROBABILITY ESTIMATES"
+    row_sums = []
+    for i in unique_words: #Calculate row sums
+        row_sums.append(sum(dframe.loc[i]))
+    row_sums = pandas.Series(row_sums, index=unique_words) #Assign row index
+
+    output_aid("Row sums", row_sums)
+    
+    for word_index in unique_words:
+        #call and modify rows
+        #dframe.loc[word_index] = round(dframe.loc[word_index]/row_sums[word_index],2)
+        
+        if(row_sums[word_index] != 0):
+            dframe.loc[word_index] = round(dframe.loc[word_index]/row_sums[word_index],2)
+        else:
+            #In the unlikely event that word_index occurs only one time,
+            #and that this happens to be at the very end of the text.
+            #This would otherwise cause this line to be NaN because the row sum
+            #becomes zero.
+            dframe.loc[word_index] = 0
+    
+    output_aid("FINAL PROBABILITY MATRIX", dframe)
+    return(dframe)
+
+
+
+def output_aid(description, thing):
+    print("\n")
+    print("--v--", description, "--v--")
+    print(thing)
+    print("--^--", description, "--^--")
+
+    
+
+make_pmatrix("Får får får? Nej får får lamm")
+
---
+++ CodeTesting/main.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 15 14:10:30 2022
+
+@author: jorst
+"""
+
+import pandas
+import GGG_probmatrix as GGG
+
+
+
+
+
+
+
+#df = pandas.read_csv("Text_test.txt")
+
+df = pandas.read_csv("Text_test.txt")
+
+print(df[0])
+
+#df["Textcolumn"]
+
+#df.loc[0]
+
+
+#GGG.make_pmatrix(df.to_string())
+
+
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+df = pandas.DataFrame(data)
+
+df
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
