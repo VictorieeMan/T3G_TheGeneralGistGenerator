c6751c359b1b78cdd24cdf7701be37b7ebdae1de
Simon Jorstedt <jorstedtsimon@gmail.com>
2022-04-16 13:10:20 +0200

    Wrote basic functionality for Clean_data()
    Clean_data() in Gist_generator.py now has basic functionality. It can read a text file, combine it and do some basic text cleaning. Created new text file (TESTING.txt) for testing and made some changes in Text_test.txt

--- CodeTesting/Gist_generator.py
+++ CodeTesting/Gist_generator.py
@@ -5,15 +5,31 @@ test_text = "This should be some string containing some words repeated over and 
 
 #GGG_prob.make_pmatrix(test_text)
 
-def Clean_data(text_input): #Output: likely just STRING
-    "This function will take an input, either raw text or a dataframe,"
-    "or maybe even filename, and then clean it and prepare it for"
-    "analysis."
-    "This procedure will include:"
-    "Removing certain characters, like some punctuations"
-    "Combining many input strings into a single string ready for analysis."
-    "De-capitalizing letters"
-    "Inserting a \"text-stop\" identifying character after all data strings."
+def Clean_data(file_name): #Output: likely just STRING
+    '''
+    This function will take an input, either raw text or a dataframe,
+    or maybe even filename, and then clean it and prepare it for analysis.
+    
+    This procedure will include:
+    Removing certain characters, like some punctuations
+    Combining many input strings into a single string ready for analysis.
+    De-capitalizing letters
+    Inserting a \"text-stop\" identifying character after all data strings.
+    '''
+
+    #deprecated, until we proceed to read data with SQL
+    #df = pandas.read_csv(file_name, header=None)
+
+    #filepath = "T3G_TheGeneralGistGenerator\CodeTesting\\" + file_name
+    file = open(file_name, "r")
+    combined_text = "".join(file.read()) #combine all text in the file
+    
+    #Cleaning text
+    combined_text = combined_text.lower()
+    combined_text = combined_text.replace("\n", " ")
+
+    print(combined_text)
+    print("completed")
 
 def generate_gist(text_input):
     #Clean_data()
@@ -22,4 +38,6 @@ def generate_gist(text_input):
     print("Finished generating gist")
     #print(pmatrix)
 
-generate_gist(test_text)
+#generate_gist(test_text)
+
+Clean_data("CodeTesting/Text_test.txt")
